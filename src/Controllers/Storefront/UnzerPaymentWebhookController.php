c6699fb3ef64c03a26ea9e4ce167c973b4a74773
Mike Stuebbe <mike.stuebbe@metacrew.de>
2023-11-22 18:19:45 +0100

    FAV-1457 debugging... add delay in controller

--- src/Controllers/Storefront/UnzerPaymentWebhookController.php
+++ src/Controllers/Storefront/UnzerPaymentWebhookController.php
@@ -46,6 +46,7 @@ class UnzerPaymentWebhookController extends StorefrontController
     public function execute(Request $request, SalesChannelContext $salesChannelContext): Response
     {
 
+        sleep(5);
         // ToDo: remove! debugging FAV-1457
         $this->logger->error('BOOOOOOOOOOOOOOOOOOM ('.getmypid().') The webhook triggered.');
 
